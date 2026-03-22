ab6fd3db4b3c3010d25423c8ee345e3078dc944b
BharathKumarReddy <nbkumar2103@gmail.com>
2026-03-22 18:52:45 +0530

    updated file

--- 40-eks/locals.tf
+++ 40-eks/locals.tf
@@ -1,11 +1,7 @@
-locals {
+locals{
+    name = "${var.project_name}-${var.environment}"
     vpc_id = data.aws_ssm_parameter.vpc_id.value
-    eks_node_sg_id = data.aws_ssm_parameter.eks_node_sg_id.value
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     eks_control_plane_sg_id = data.aws_ssm_parameter.eks_control_plane_sg_id.value
-    private_subnet_ids = split ("," , data.aws_ssm_parameter.private_subnet_ids.value)
-    common_tags = {
-        Project = var.project
-        Environment = var.environment
-        Terraform = "true"
-    }
+    eks_node_sg_id = data.aws_ssm_parameter.eks_node_sg_id.value
 }
